b84697d7d18f63a8feb260aa111e9fba90be6d96
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-25 13:58:04 +0200

    Building a Custom Cell Class for TableView

---
+++ DessertTable/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  DessertTable
+//
+//  Created by Yan Malinovsky on 25.11.16.
+//  Copyright © 2016 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet var cellImageView: UIImageView!
+    
+    @IBOutlet var cellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
