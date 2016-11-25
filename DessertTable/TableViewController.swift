49592918886fe8d64b2f000190fe014f0f16c3d6
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-25 15:45:54 +0200

    Displaying Table Rows with Images and Label

--- DessertTable/TableViewController.swift
+++ DessertTable/TableViewController.swift
@@ -43,23 +43,26 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return foodArray.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
 
-        // Configure the cell...
+        let foodItem = foodArray[indexPath.row]
 
+        cell.cellImageView.image = UIImage(named: foodItem.imageName)
+        cell.cellLabel.text = foodItem.description
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
